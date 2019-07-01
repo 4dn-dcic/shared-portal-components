d38ec8664e44d2e974f2f1f5027aeea697b22547
Alex Balashov <alexander_balashov@hms.harvard.edu>
2019-07-01 17:38:03 -0400

    Some refactoring

--- src/components/ui/ItemFooterRow.js
+++ src/components/ui/ItemFooterRow.js
@@ -4,7 +4,6 @@ import React from 'react';
 import _ from 'underscore';
 import { console } from './../util';
 
-/** TODO: Split into functional components, cleanup */
 
 /**
  * Reusable Component for showing Aliases, External References, etc.
@@ -15,137 +14,97 @@ import { console } from './../util';
  * @prop {Object} context - JSON representation of current Item object. Should be available through Redux store's context.
  * @prop {Object} schemas - JSON representation of sitewide schemas.
  */
-export class ItemFooterRow extends React.Component {
+export const ItemFooterRow = React.memo(function ItemFooterRow({ context, schemas }){
+    const { aliases = [], actions = [], external_references = [], alternate_accessions = [] } = context || {};
 
-    constructor(props){
-        super(props);
-        this.aliases                = this.aliases.bind(this);
-        this.alternateAccessions    = this.alternateAccessions.bind(this);
-        this.externalReferences     = this.externalReferences.bind(this);
+    if (external_references.length === 0 && alternate_accessions.length === 0){
+        return null;
     }
 
-    /**
-     * Render list of aliases for this item, wrapped in a <div>.
-     * Only show if (a) any aliases exist AND (b) current user has edit access (e.g. submitter, admin).
-     *
-     * @returns {JSX.Element} Div React element containing aliases list or null.
-     */
-    aliases(){
-        if (
-            !this.props.context
-            || !Array.isArray(this.props.context.aliases)
-            || this.props.context.aliases.length === 0
-        ) return null;
-        if (!Array.isArray(this.props.context.actions)) return null;
-        if (!_.find(this.props.context.actions, { 'name' : 'edit' })) return null; // No 'Edit' action for this Item.
+    return (
+        <div className="row">
 
-        var aliases = this.props.context.aliases.length > 0 ? this.props.context.aliases : [<em>None</em>];
-        return (
+            <hr className="mb-08 mt-1"/>
+
+            <ExternalReferencesSection context={context} externalReferences={external_references} />
+
+            {/* <AliasesSection context={context} aliases={aliases} actions={actions} /> */}
+
+            <AlternateAccessionSection context={context} alternateAccessions={alternate_accessions} />
+
+        </div>
+    );
+});
+
+
+function ExternalReferencesSection({ alternateAccessions }){
+    if (externalReferences.length === 0){
+        return null;
+    }
+    return (
+        <div className="col col-xs-12 col-md-6">
+            <h4 className="text-300">External References</h4>
             <div>
-                <h4 className="text-500">Aliases</h4>
-                <div>
-                    <ul>
-                        { aliases.map(function(alias, i){
-                            return (
-                                <li key={i}>{ alias }</li>
-                            );
-                        }) }
-                    </ul>
-                </div>
+                <ul>
+                    { _.map(externalReferences, function(extRef, i){
+                        return (
+                            <li key={i}>
+                                { typeof extRef.ref === 'string' ?
+                                    <ExternalReferenceLink uri={extRef.uri || null} children={extRef.ref} />
+                                    :
+                                    extRef
+                                }
+
+                            </li>
+                        );
+                    }) }
+                </ul>
             </div>
-        );
-    }
-
-    alternateAccessions(){
-        if (
-            !this.props.context
-            || !Array.isArray(this.props.context.alternate_accessions)
-            || this.props.context.alternate_accessions.length === 0
-        ) return null;
-        var alternateAccessions = this.props.context.alternate_accessions.length > 0 ? this.props.context.alternate_accessions : [<em>None</em>];
-        return (
-            <div>
-                <h4 className="text-300">Alternate Accessions</h4>
-                <div>
-                    <ul>
-                        { alternateAccessions.map(function(alias, i){
-                            return (
-                                <li key={i}>{ alias }</li>
-                            );
-                        }) }
-                    </ul>
-                </div>
-            </div>
-        );
-    }
-
-    externalReferences(schemas){
-        if (
-            !this.props.context
-            || !Array.isArray(this.props.context.external_references)
-            || this.props.context.external_references.length === 0
-        ) return null;
-        var externalRefs = this.props.context.external_references.length > 0 ? this.props.context.external_references : [<em>None</em>];
-        return (
-            <div>
-                <h4 className="text-300">External References</h4>
-                <div>
-                    <ul>
-                        { externalRefs.map(function(extRef, i){
-                            return (
-                                <li key={i}>
-                                    { typeof extRef.ref === 'string' ?
-                                        <ExternalReferenceLink uri={extRef.uri || null} children={extRef.ref} />
-                                        :
-                                        extRef
-                                    }
-
-                                </li>
-                            );
-                        }) }
-                    </ul>
-                </div>
-            </div>
-        );
-    }
-
-    render() {
-        var { schemas, context } = this.props;
-
-        var externalReferences  = this.externalReferences(schemas || {}),
-            aliases             = this.aliases(),
-            alternateAccessions = this.alternateAccessions();
-
-        if (!externalReferences && !alternateAccessions) return null;
-
-        return (
-            <div className="row">
-
-                <hr className="mb-08 mt-1"/>
-
-                { externalReferences ?
-                    <div className="col-xs-12 col-md-6">
-                        { externalReferences }
-                    </div>
-                    : null }
-
-                {/* aliases ?
-                <div className="col-xs-12 col-md-4">
-                    { aliases }
-                </div>
-                : null */}
-
-                { alternateAccessions ?
-                    <div className="col-xs-12 col-md-6">
-                        { alternateAccessions }
-                    </div>
-                    : null }
-
-            </div>
-        );
-
-    }
+        </div>
+    );
 }
+
+function AlternateAccessionSection({ context }){
+    if (alternateAccessions.length === 0){
+        return null;
+    }
+    return (
+        <div className="col col-xs-12 col-md-6">
+            <h4 className="text-300">Alternate Accessions</h4>
+            <div>
+                <ul>
+                    { _.map(alternateAccessions, function(alias, i){
+                        return (
+                            <li key={i}>{ alias }</li>
+                        );
+                    }) }
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+function AliasesSection({ aliases, actions }){
+    if (aliases.length === 0) return null;
+
+    if (!_.find(actions, { 'name' : 'edit' })) return null; // No 'Edit' action for this Item.
+
+    return (
+        <div>
+            <h4 className="text-500">Aliases</h4>
+            <div>
+                <ul>
+                    { _.map(aliases, function(alias, i){
+                        return (
+                            <li key={i}>{ alias }</li>
+                        );
+                    }) }
+                </ul>
+            </div>
+        </div>
+    );
+}
+
 
 /**
  * Used to display an external reference link.
