ecf4bd6faba370bdee7a98ff0b47a71ce11ed1c1
Alex Balashov <alexander_balashov@hms.harvard.edu>
2019-07-16 17:05:08 -0400

    Polyfill for require.ensure for NPM tests

--- src/components/navigation/components/LoginController.js
+++ src/components/navigation/components/LoginController.js
@@ -15,6 +15,12 @@ import { event as trackEvent } from './../../util/analytics';
 /** Imported in componentDidMount. */
 let Auth0Lock = null;
 
+// Manual polyfill for NPM tests, @see https://github.com/facebook/create-react-app/issues/1064
+if (!require.ensure) {
+    console.error("No require.ensure present - \nFine if within an NPM test, error if in browser/webpack context.");
+    require.ensure = (deps, cb) => cb(require);
+}
+
 /** Controls Login process, also shows Registration Modal */
 export class LoginController extends React.PureComponent {
 
