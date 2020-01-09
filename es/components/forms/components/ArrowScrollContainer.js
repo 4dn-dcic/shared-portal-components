6b8636e102f6600b424247e81eb94919dfa13c4c
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2020-01-09 17:29:10 -0500

    semi-functional :/

--- es/components/forms/components/ArrowScrollContainer.js
+++ es/components/forms/components/ArrowScrollContainer.js
@@ -9,6 +9,8 @@ var _react = _interopRequireDefault(require("react"));
 
 var _propTypes = _interopRequireDefault(require("prop-types"));
 
+var _underscore = require("underscore");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function (obj) { return typeof obj; }; } else { _typeof = function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
@@ -21,75 +23,161 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function (o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function (o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 var VerticalScrollContainer =
 /*#__PURE__*/
 function (_React$PureComponent) {
   _inherits(VerticalScrollContainer, _React$PureComponent);
 
-  function VerticalScrollContainer() {
+  function VerticalScrollContainer(props) {
+    var _this;
+
     _classCallCheck(this, VerticalScrollContainer);
 
-    return _possibleConstructorReturn(this, _getPrototypeOf(VerticalScrollContainer).apply(this, arguments));
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(VerticalScrollContainer).call(this, props));
+    _this.state = {
+      hasOverflow: false,
+      canScrollUp: false,
+      canScrollDown: false
+    };
+    _this.scrollContainer = _react["default"].createRef();
+    _this.handleScrollDown = _this.handleScrollDown.bind(_assertThisInitialized(_this));
+    _this.handleScrollUp = _this.handleScrollUp.bind(_assertThisInitialized(_this));
+    _this.performScrollAction = _this.performScrollAction.bind(_assertThisInitialized(_this));
+    _this.checkForOverflow = _this.checkForOverflow.bind(_assertThisInitialized(_this));
+    _this.checkForScrollPosition = _this.checkForScrollPosition.bind(_assertThisInitialized(_this));
+    _this.debounceCheckforOverflow = (0, _underscore.debounce)(_this.checkForOverflow, 200, false);
+    _this.debounceCheckForScrollPosition = (0, _underscore.debounce)(_this.checkForScrollPosition, 200, true);
+    return _this;
   }
 
   _createClass(VerticalScrollContainer, [{
-    key: "render",
-    // static defaultProps = {
-    //     'scrollRate' : 10
-    // }
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         scrollPosition: 0,
-    //     };
-    //     this.scrolling = false;
-    // }
-    // handleScrollUpHoverStart(e) {
-    //     this.scrolling = true;
-    //     this.performScrollAction("up");
-    // }
-    // handleScrollDownHoverStart(e) {
-    //     this.scrolling = true;
-    //     this.performScrollAction("down");
-    // }
-    // handleScrollDownHoverEnd(e) {
-    //     this.scrolling = false;
-    // }
-    // performScrollAction(direction) {
-    //     // todo: handle scroll
-    // }
-    value: function render() {
-      var _this$props = this.props,
-          _this$props$items = _this$props.items,
-          items = _this$props$items === void 0 ? [] : _this$props$items,
-          header = _this$props.header,
-          footer = _this$props.footer; // const { scrollPosition } = this.state;
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      this.checkForOverflow();
+      this.checkForScrollPosition();
+      this.scrollContainer.current.addEventListener('scroll', this.debounceCheckForScrollPosition);
+    }
+  }, {
+    key: "componentDidUpdate",
+    value: function componentDidUpdate(prevProps) {
+      var _this$props$items = this.props.items,
+          items = _this$props$items === void 0 ? [] : _this$props$items;
+      var _prevProps$items = prevProps.items,
+          prevItems = _prevProps$items === void 0 ? [] : _prevProps$items;
 
-      console.log(items);
-      var enoughItems = items.length > 5; // todo: update to take into account individual item heights
+      if (prevItems.length !== items.length) {
+        this.checkForOverflow();
+        this.checkForScrollPosition();
+      }
+    }
+  }, {
+    key: "componentWillUnmount",
+    value: function componentWillUnmount() {
+      this.scrollContainer.current.removeEventListener('scroll', this.debounceCheckForScrollPosition);
+      this.debounceCheckforOverflow.cancel();
+    }
+  }, {
+    key: "checkForScrollPosition",
+    value: function checkForScrollPosition() {
+      var _this$scrollContainer = this.scrollContainer.current,
+          scrollTop = _this$scrollContainer.scrollTop,
+          scrollHeight = _this$scrollContainer.scrollHeight,
+          clientHeight = _this$scrollContainer.clientHeight; // console.log(this.scrollContainer.current);
+
+      console.log("scrollHeight: ", scrollHeight);
+      console.log("clientHeight: ", clientHeight);
+      console.log("scrollTop", scrollTop);
+      this.setState({
+        canScrollUp: scrollTop > 0,
+        canScrollDown: scrollTop !== scrollHeight - clientHeight
+      });
+    }
+  }, {
+    key: "checkForOverflow",
+    value: function checkForOverflow() {
+      console.log("checkforoverflow"); // take into account individual item heights
       // and see how many will fill the container
 
-      // todo: add some logic
+      var _this$scrollContainer2 = this.scrollContainer.current,
+          scrollHeight = _this$scrollContainer2.scrollHeight,
+          clientHeight = _this$scrollContainer2.clientHeight;
+      this.setState({
+        hasOverflow: scrollHeight > clientHeight
+      });
+    }
+  }, {
+    key: "handleScrollUp",
+    value: function handleScrollUp() {
+      console.log("handleScrollUp");
+      this.scrolling = true;
+      this.performScrollAction("up");
+    }
+  }, {
+    key: "handleScrollDown",
+    value: function handleScrollDown() {
+      console.log("handleScrollDown");
+      this.scrolling = true;
+      this.performScrollAction("down");
+    }
+  }, {
+    key: "performScrollAction",
+    value: function performScrollAction(direction) {
+      var scrollRate = this.props.scrollRate;
+      var nonAbsScrollRate = direction === "up" ? scrollRate : -scrollRate;
+      this.scrollContainer.current.scrollBy({
+        behavior: 'smooth',
+        up: nonAbsScrollRate
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this$props = this.props,
+          _this$props$items2 = _this$props.items,
+          items = _this$props$items2 === void 0 ? [] : _this$props$items2,
+          header = _this$props.header,
+          footer = _this$props.footer;
+      var _this$state = this.state,
+          hasOverflow = _this$state.hasOverflow,
+          canScrollUp = _this$state.canScrollUp,
+          canScrollDown = _this$state.canScrollDown;
       return _react["default"].createElement("div", {
         className: "arrow-scroll-container"
-      }, null, _react["default"].createElement("div", {
-        className: "scrollable-list-container"
+      }, hasOverflow && canScrollUp ? _react["default"].createElement("button", {
+        className: "button-scroll arrow-up d-block text-center w-100",
+        style: {
+          boxShadow: "0 10px 10px 3px rgba(200,200,200,0.2)",
+          color: "#cccccc",
+          border: "none"
+        },
+        onClick: this.handleScrollUp,
+        type: "button"
+      }, _react["default"].createElement("i", {
+        className: "icon fas icon-angle-up"
+      })) : null, _react["default"].createElement("div", {
+        className: "scrollable-list-container",
+        ref: this.scrollContainer
       }, _react["default"].createElement("ul", {
         className: "scroll-items list-unstyled mb-0 py-2"
-      }, header, items, footer)), enoughItems && true ? _react["default"].createElement("a", {
-        className: "button-scroll arrow-down d-block text-center",
+      }, header, items, footer)), hasOverflow && canScrollDown ? _react["default"].createElement("button", {
+        className: "button-scroll arrow-down d-block text-center w-100",
         style: {
           boxShadow: "0 -10px 10px 30px rgba(200,200,200,0.2)",
-          color: "#cccccc"
-        }
+          color: "#cccccc",
+          border: "none"
+        },
+        onClick: this.handleScrollDown,
+        type: "button"
       }, _react["default"].createElement("i", {
         className: "icon fas icon-angle-down"
       })) : null);
@@ -100,7 +188,13 @@ function (_React$PureComponent) {
 }(_react["default"].PureComponent);
 
 exports.VerticalScrollContainer = VerticalScrollContainer;
+
+_defineProperty(VerticalScrollContainer, "defaultProps", {
+  'scrollRate': 10
+});
+
 VerticalScrollContainer.propTypes = {
+  scrollRate: _propTypes["default"].number,
   items: _propTypes["default"].arrayOf(_propTypes["default"].any),
   header: _propTypes["default"].any,
   footer: _propTypes["default"].any
--- src/components/forms/components/ArrowScrollContainer.js
+++ src/components/forms/components/ArrowScrollContainer.js
@@ -1,78 +1,136 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { debounce } from 'underscore';
 
 export class VerticalScrollContainer extends React.PureComponent {
 
-    // static defaultProps = {
-    //     'scrollRate' : 10
-    // }
+    static defaultProps = {
+        'scrollRate' : 10
+    }
 
-    // constructor(props) {
-    //     super(props);
+    constructor(props) {
+        super(props);
 
-    //     this.state = {
-    //         scrollPosition: 0,
-    //     };
+        this.state = {
+            hasOverflow: false,
+            canScrollUp: false,
+            canScrollDown: false
+        };
 
-    //     this.scrolling = false;
-    // }
+        this.scrollContainer = React.createRef();
 
-    // handleScrollUpHoverStart(e) {
-    //     this.scrolling = true;
-    //     this.performScrollAction("up");
-    // }
+        this.handleScrollDown = this.handleScrollDown.bind(this);
+        this.handleScrollUp = this.handleScrollUp.bind(this);
+        this.performScrollAction = this.performScrollAction.bind(this);
+        this.checkForOverflow = this.checkForOverflow.bind(this);
+        this.checkForScrollPosition = this.checkForScrollPosition.bind(this);
 
-    // handleScrollDownHoverStart(e) {
-    //     this.scrolling = true;
-    //     this.performScrollAction("down");
-    // }
+        this.debounceCheckforOverflow = debounce(this.checkForOverflow, 200, false);
+        this.debounceCheckForScrollPosition = debounce(this.checkForScrollPosition, 200, true);
+    }
 
-    // handleScrollDownHoverEnd(e) {
-    //     this.scrolling = false;
-    // }
+    componentDidMount() {
+        this.checkForOverflow();
+        this.checkForScrollPosition();
 
-    // performScrollAction(direction) {
-    //     // todo: handle scroll
-    // }
+        this.scrollContainer.current.addEventListener('scroll', this.debounceCheckForScrollPosition);
+    }
+
+    componentDidUpdate(prevProps) {
+        const { items = [] } = this.props;
+        const { items : prevItems = [] } = prevProps;
+        if (prevItems.length !== items.length) {
+            this.checkForOverflow();
+            this.checkForScrollPosition();
+        }
+    }
+
+    componentWillUnmount() {
+        this.scrollContainer.current.removeEventListener('scroll',
+            this.debounceCheckForScrollPosition);
+        this.debounceCheckforOverflow.cancel();
+    }
+
+    checkForScrollPosition() {
+        const { scrollTop, scrollHeight, clientHeight } = this.scrollContainer.current;
+        // console.log(this.scrollContainer.current);
+        console.log("scrollHeight: ", scrollHeight);
+        console.log("clientHeight: ", clientHeight);
+        console.log("scrollTop", scrollTop);
+        this.setState({
+            canScrollUp : scrollTop > 0,
+            canScrollDown: scrollTop !== scrollHeight - clientHeight
+        });
+    }
+
+    checkForOverflow() {
+        console.log("checkforoverflow");
+        // take into account individual item heights
+        // and see how many will fill the container
+        const { scrollHeight, clientHeight } = this.scrollContainer.current;
+        const hasOverflow = scrollHeight > clientHeight;
+        this.setState({ hasOverflow });
+    }
+
+    handleScrollUp(e) {
+        console.log("handleScrollUp");
+        this.scrolling = true;
+        this.performScrollAction("up");
+    }
+
+    handleScrollDown(e) {
+        console.log("handleScrollDown");
+        this.scrolling = true;
+        this.performScrollAction("down");
+    }
+
+    performScrollAction(direction) {
+        const { scrollRate } = this.props;
+        const nonAbsScrollRate = direction === "up" ? scrollRate : -scrollRate;
+        const scrollSettings = { behavior: 'smooth', up: nonAbsScrollRate };
+        this.scrollContainer.current.scrollBy(scrollSettings);
+    }
 
     render() {
         const { items = [], header, footer } = this.props;
-        // const { scrollPosition } = this.state;
-        console.log(items);
-        const enoughItems = items.length > 5;
-        // todo: update to take into account individual item heights
-        // and see how many will fill the container
-
-        const showUpButton = false; // todo: confirm logic is correct
-        const showDownButton = enoughItems && true; // todo: add some logic
-
+        const { hasOverflow, canScrollUp, canScrollDown } = this.state;
 
         return (
             <div className="arrow-scroll-container">
-                { showUpButton ?
-                    <a className="button-scroll arrow-up d-block text-center"
-                        style={{ boxShadow: "0 10px 10px 3px rgba(200,200,200,0.2)", color: "#cccccc" }}>
+                { hasOverflow && canScrollUp ?
+                    <button className="button-scroll arrow-up d-block text-center w-100"
+                        style={{
+                            boxShadow: "0 10px 10px 3px rgba(200,200,200,0.2)",
+                            color: "#cccccc",
+                            border: "none"
+                        }}
+                        onClick={this.handleScrollUp} type="button">
                         <i className="icon fas icon-angle-up"></i>
-                    </a>
+                    </button>
                     : null }
-                <div className="scrollable-list-container">
+                <div className="scrollable-list-container" ref={this.scrollContainer}>
                     <ul className="scroll-items list-unstyled mb-0 py-2">
                         { header }
                         { items }
                         { footer }
                     </ul>
                 </div>
-                { showDownButton ?
-                    <a className="button-scroll arrow-down d-block text-center"
-                        style={{ boxShadow: "0 -10px 10px 30px rgba(200,200,200,0.2)", color: "#cccccc" }}>
+                { hasOverflow && canScrollDown ?
+                    <button className="button-scroll arrow-down d-block text-center w-100"
+                        style={{
+                            boxShadow: "0 -10px 10px 30px rgba(200,200,200,0.2)",
+                            color: "#cccccc",
+                            border: "none"
+                        }} onClick={this.handleScrollDown} type="button">
                         <i className="icon fas icon-angle-down"></i>
-                    </a>
+                    </button>
                     : null }
             </div>
         );
     }
 }
 VerticalScrollContainer.propTypes = {
+    scrollRate: PropTypes.number,
     items: PropTypes.arrayOf(PropTypes.any),
     header: PropTypes.any,
     footer: PropTypes.any
