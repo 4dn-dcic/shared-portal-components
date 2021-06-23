4bc311a46f71c49cace1b3490e6b66fb06e2aa55
Alex Balashov <alexander_balashov@hms.harvard.edu>
2021-06-23 18:48:23 -0400

    Re-implement IndeterminateCheckbox...
    now updates everytime ref changes using callback
    ref function.

--- src/components/forms/components/IndeterminateCheckbox.js
+++ src/components/forms/components/IndeterminateCheckbox.js
@@ -1,38 +1,10 @@
 import React from 'react';
-import _ from 'underscore';
 
-export class IndeterminateCheckbox extends React.PureComponent {
-
-    constructor(props){
-        super(props);
-        this.setIndeterminateOnRef = this.setIndeterminateOnRef.bind(this);
-        this.checkboxRef = React.createRef();
-    }
-
-    componentDidMount(){
-        const { indeterminate } = this.props;
-        // Can be skipped if not set to true.
-        if (indeterminate === true){
-            this.setIndeterminateOnRef();
-        }
-    }
-
-    componentDidUpdate({ indeterminate: pastIndeterminate }){
-        const { indeterminate } = this.props;
-        if (pastIndeterminate !== indeterminate){
-            this.setIndeterminateOnRef();
-        }
-    }
-
-    setIndeterminateOnRef(){
-        const { indeterminate } = this.props;
-        if (this.checkboxRef.current){
-            this.checkboxRef.current.indeterminate = indeterminate;
-        }
-    }
-
-    render(){
-        const { indeterminate, ...passProps } = this.props;
-        return <input type="checkbox" {...passProps} ref={this.checkboxRef} />;
-    }
+export function IndeterminateCheckbox (props) {
+    const { indeterminate = false, ...passProps } = props;
+    // See https://reactjs.org/docs/refs-and-the-dom.html#callback-refs
+    const callbackRef = function(el){
+        if (indeterminate && el) el.indeterminate = indeterminate;
+    };
+    return <input type="checkbox" {...passProps} ref={callbackRef} />;
 }
