b09cca544eac14878c14d9375bcb32ef8afae637
Alex Balashov <alexander_balashov@hms.harvard.edu>
2021-11-08 15:00:18 -0500

    Remove /es/ from git

--- es/components/browse/components/table-commons/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-export { basicColumnExtensionMap, DEFAULT_WIDTH_MAP, TableRowToggleOpenButton, // Might be useful if defining some wicked custom tables.
-DisplayTitleColumnWrapper, DisplayTitleColumnDefault, DisplayTitleColumnUser } from './basicColumnExtensionMap';
-export { ColumnCombiner, columnsToColumnDefinitions, // Should be private function but still used by 4DN ItemPageTable
-haveContextColumnsChanged } from './ColumnCombiner';
-export { HeadersRow, flattenColumnsDefinitionsSortFields } from './HeadersRow';
-export { ResultRowColumnBlockValue, sanitizeOutputValue } from './ResultRowColumnBlockValue';
