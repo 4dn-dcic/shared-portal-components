f185f4bdc218579e3779e2c52092760896dc27df
Alex Balashov <alexander_balashov@hms.harvard.edu>
2022-03-21 11:18:41 -0400

    Update react-infinite version +..
    Remove props.href from DisplayTitleColumnWrapper

--- es/components/forms/components/DragAndDropUpload.js
+++ es/components/forms/components/DragAndDropUpload.js
@@ -1,10 +1,10 @@
 function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -38,7 +38,7 @@ export var DragAndDropUploadSubmissionViewController = /*#__PURE__*/function (_R
     return _super.apply(this, arguments);
   }
 
-  return DragAndDropUploadSubmissionViewController;
+  return _createClass(DragAndDropUploadSubmissionViewController);
 }(React.Component);
 /**
  * Main component for independent drag and drop file upload. May eventually be updated to take a prop
